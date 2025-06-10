cb66c6be9cd6038ba6372e691767d7ba04007852
Cesar Aguilera <195704999+Yissa001@users.noreply.github.com>
2025-06-09 21:21:21 -0300

    Cambios varios

--- guiAccess.c
+++ guiAccess.c
@@ -5,9 +5,14 @@
 #include <string.h> //para cadena de caracteres 
 #include <stdlib.h> //para utilizar la funcion exit()
 
+void limpiarSalto(char *cadena){
+    cadena [strcspn(cadena, "\n")]= '\0';
+}
+
 int main()
+
 {
-    char user[31], passwd[31];
+    char user[31], passwd[31], user1[31], passwd1[31];
     int choice1, choice2;
 
     printf("Bienvenido a KeepItPrivate.\n");
@@ -15,53 +20,58 @@ int main()
 
     getchar(); //Aguarda que el usuario presione enter
 
-
-    do
+    while(1)
     {
-        
-        printf("¿Que deseas hacer?: (Selecciona un numero)\n");
+        printf("Elige un número:\n");
         printf("1-) Iniciar sesión.\n");
         printf("2-) Crear nuevo usuario.\n");
-        printf("3-) Exit\n");
-        printf("\nIntroduce tu elección: ");
+        printf("3-) Cerrar aplicación.\n");
         scanf(" %d", &choice1);
+        getchar();
 
-        switch (choice1)
-        {
+        switch (choice1){
             case(1):
             {
-                printf("\nInicio de sesión.\n");
-                printf("\nIntroduce tu usuario: ");
-                getchar();
-                fgets(user, sizeof(user), stdin);
-                getchar();
-                printf("\nAhora selecciona una contraseña: ");
-                fgets(passwd, sizeof(passwd), stdin);
+                printf("Inicio de sesión\n");
+                printf("Nombre de usuario: ");
+                fgets(user1, sizeof(user1), stdin);
+                limpiarSalto(user1);
 
-                if ((user == user)&&(passwd==passwd))
+                printf("\nIntroduce la contraseña del usuario %s:", user1);
+                fgets(passwd1, sizeof(passwd1), stdin);
+                limpiarSalto(passwd1);
+
+                if ((strcmp(user1, user)==0)&&(strcmp(passwd, passwd1)==0))
                 {
                     printf("Acceso concedido.\n");
                 }
                 else
                 {
-                    printf("Contraseña o usuario incorrecto\n")
+                    printf("Usuario o contraseña incorrecta. Intente de nuevo");
                 }
                 break;
             }
-
             case(2):
             {
-            printf("Crear usuario.");
-            printf("Introduce nombre del usuario: ");
-            fgets(user, sizeof(user), stdin);
-            break;
-            }
+                printf("Creación de usuario.\n");
+                printf("Crea un usuario: ");
+                fgets(user, sizeof(user), stdin);
+                limpiarSalto(user);
 
+                printf("\nCrea una contraseña. (Debe ser de 14 dígitos, poseer 4 números y un al menos un carácter especial.)\n");
+                printf("No te preocupes si no ves lo que escribes, de esta forma esta diseñado.\n");
+                printf("Contraseña: ");
+                fgets(passwd, sizeof(passwd), stdin);
+                limpiarSalto(passwd);
+
+                break;
+            }
             case(3):
-                exit(1);
+            {
+                exit(0);
+            }
         }
     }
-    while (choice1!=3);
-
     return (0);
+
 }
